8cbdcc0eb914805e05e232042496d38036e16467
SABARISHVELAYUDHAN <sabarishvelayudhan121@gmail.com>
2022-08-26 23:28:33 +0530

    update

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ function myFunction(){
         let data = await response.json()
         document.getElementById("demo").innerHTML = data.origin;
         return data;
+
+
     }
         
     getIP().then(data => console.log(data));
+
+    document.getElementById("demo").innerHTML = window.alert("error");
+
 }
+
+function myFunction1(){
+        
+        async function postName(){
+            let response = await fetch ("http://httpbin.org/post")
+            let data = await response.json()
+            document.getElementById("demo1").innerHTML = "hello";
+            request.send("http://httpbin.org/post", "hello");
+
+    }
+
+}
+
